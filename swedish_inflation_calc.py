776a21cc079f6ca7b4b3b6db7e0c51fbb3515dd2
Alexander Meldrum <alexander.meldrum@zenseact.com>
2024-02-27 10:25:08 +0100

    2024 Jan update

--- swedish_inflation_calc.py
+++ swedish_inflation_calc.py
@@ -12,11 +12,11 @@ from statistics import mean
 start_month = 8
 start_year = 2019
 start_salary = 40000
-current_salary = 44600
+current_salary = 48400
 ### Current Date ###
 # Can only be set to latest valid inflation data point in inflation_by_month below.
-current_month = 3
-current_year = 2023
+current_month = 1
+current_year = 2024
 #current_month = int(datetime.today().strftime('%m'))
 #current_year = int(datetime.today().strftime('%Y'))
 #----------------------------------------------------------------------#
@@ -32,7 +32,8 @@ inflation_by_month = {
     '2020':	[1.2, 1.0, 0.6, -0.4, 0.0, 0.7, 0.5, 0.7, 0.3, 0.3, 0.2, 0.5],
     '2021':	[1.7, 1.5, 1.9,	2.5, 2.1, 1.6, 1.7,	2.4, 2.8, 3.1, 3.6, 4.1],
     '2022':	[3.9, 4.5, 6.1,	6.4, 7.2, 8.5, 8.0,	9.0, 9.7, 9.3, 9.5,	10.2],
-    '2023':	[9.3, 9.4, 8.0],   # TODO Update me later
+    '2023':	[9.3, 9.4, 8.0, 7.6, 6.7, 6.4, 6.4 ,4.7, 4.0, 4.2, 3.6, 2.3],   
+    '2024':	[3.3, ], # TODO Update me later
 }
 
 ### Prepare the loop ###
